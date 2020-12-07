a4be1222d39919aa83c241779e6b98684a9a88b8
madeleineenberg <madeleine_enberg@hotmail.com>
2020-12-07 10:45:34 +0100

    styling with candycane text and snowstorm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
 import CardList from './components/CardList';
+import SnowStorm from 'react-snowstorm';
 
 function App() {
   return (
     <div className="app-wrapper">
+      <SnowStorm/>
       <h1 className="title">Funny Gif</h1>
     <CardList/>
     </div>
